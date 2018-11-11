ae4611c27bc712cc91e42c0416ff21e5bf50c728
wataru-watanabe <tao6128@gmail.com>
2018-11-11 17:27:16 +0900

    Updates

---
+++ precache-manifest.2dc7885f750612d5d82a0a619c5c343c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/infostock/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "825ade0efaf48f644400",
+    "url": "/infostock/static/js/runtime~main.825ade0e.js"
+  },
+  {
+    "revision": "e6353498bf100ba500f8",
+    "url": "/infostock/static/js/main.e6353498.chunk.js"
+  },
+  {
+    "revision": "1a2fe23bbee4e72efb16",
+    "url": "/infostock/static/js/1.1a2fe23b.chunk.js"
+  },
+  {
+    "revision": "e6353498bf100ba500f8",
+    "url": "/infostock/static/css/main.15a9b4b9.chunk.css"
+  },
+  {
+    "revision": "6725512245fab0fb95c9a9d693a7c9b8",
+    "url": "/infostock/index.html"
+  }
+];
